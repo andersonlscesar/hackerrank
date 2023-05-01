e67292da94e716241554aec13d72b3cf6e7f0076
Anderson César <andersonlscesar@gmail.com>
2023-05-01 14:53:41 -0300

    First Exercise
    This exercise consists to creating a function to sum two values

---
+++ php/easy/1_solve_me_first.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Complete the function solveMeFirst to compute the sum of two integers.
+ * @param int $a 
+ * @param int $b 
+ * @return int
+ */
+
+ function sum( int $a, int $b ): int 
+ {
+    return $a + $b;
+ }
