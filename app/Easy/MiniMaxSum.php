772ab57a12808e8593a7da2a9e075ac957622c09
andersonlscesar <andersonlscesar@gmail.com>
2023-10-25 19:28:01 -0300

    almost solving this exercise

--- app/Easy/MiniMaxSum.php
+++ app/Easy/MiniMaxSum.php
@@ -3,29 +3,43 @@ namespace App\Easy;
 
 class MiniMaxSum 
 {
-    public static function miniMaxSum( array $arr ): void
+    public static function miniMaxSum( array $arr )
     {
         $lower = 0;
         $max = 0;
         $sumLower = 0;
         $sumMax = 0;
-
-        foreach ( $arr as $value ) {
-            if ( $value >= $max ) $max = $value;
-            $lower = $max;            
-        }
-
-        foreach ( $arr as $value ) {
-            if ( $value <= $lower ) $lower = $value;
-        }
-
-        foreach ( $arr as $value ) { 
-            if ( $value !== $max ) $sumLower += $value;
-            if ( $value !== $lower ) $sumMax += $value;
-        }
+        $isSameContentArray = count(array_unique( $arr )) === 1;
         
-        print "$sumLower $sumMax";
+        if ($isSameContentArray) {
+            
+            for ($i = 0; $i < count($arr); $i++) {
+                if ($i > 1) {
+                    $sumMax += $arr[$i];
+                    $sumLower += $arr[$i];
+                };                
+            }
+
+        } else {
+
+            foreach ( $arr as $value ) {
+                if ( $value > $max ) $max = $value;
+                $lower = $max;            
+            }
+    
+            foreach ( $arr as $value ) {
+                if ( $value < $lower ) $lower = $value;
+            }
+    
+            foreach ( $arr as $value ) { 
+                if ( $value !== $max ) $sumLower += $value;
+                if ( $value !== $lower ) $sumMax += $value;
+            }
+        }
+
+        return "$sumLower $sumMax";
     }
 }
 
-MiniMaxSum::miniMaxSum([1, 3, 5, 7, 9]);
+// MiniMaxSum::miniMaxSum([1, 3, 5, 7, 9]);
+print MiniMaxSum::miniMaxSum([3, 3, 3, 3, 3]);
--- tests/Easy/MiniMaxSumTest.php
+++ tests/Easy/MiniMaxSumTest.php
@@ -7,10 +7,30 @@ use PHPUnit\Framework\TestCase;
 class MiniMaxSumTest extends TestCase
 {
     public function testSumFourOfTheFiveIntegers() 
+    {        
+        $result = MiniMaxSum::miniMaxSum([1, 3, 5, 7, 9]);
+        $expected = 16 . ' ' . 24;
+        $this->assertEquals($expected, $result);        
+    }
+
+    public function testSumWithNegativeNumbers() 
     {
-        ob_start();
-        MiniMaxSum::miniMaxSum([1, 3, 5, 7, 9]);
-        $output = ob_get_clean();
-        $this->expectOutputString($output);
+        $result = MiniMaxSum::miniMaxSum([-1, -3, -5, -7, -9]);
+        $expected = -25 . ' ' . -16;
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testWithLargeNumbers()
+    {
+        $result = MiniMaxSum::miniMaxSum([539674108, 549382170, 270968351, 746219035, 140597628]);
+        $expected = 1500622257 . ' ' . 2106243664;
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testWithSameNumbers() 
+    {
+        $result = MiniMaxSum::miniMaxSum([3, 3, 3, 3, 3]);
+        $expected = 9 . ' ' . 9;
+        $this->assertEquals($expected, $result);
     }
 }
